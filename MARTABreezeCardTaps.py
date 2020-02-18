8c78fef2dccfef5ae7caf4f1a50b8683b7300f12
Andi Folgado <ac.folgado@gmail.com>
2020-02-18 22:33:56 +0000

    MARTA Breeze Card Taps code challenge
    MARTABreezeCardTaps.py was my initial answer.
    
    marta.py is the answer I submitted for this code challenge. It also includes unittests that I did together with my partner, Peter - where he wrote the tests and I wrote the code to solve the challenge.

--- MARTABreezeCardTaps.py
+++ MARTABreezeCardTaps.py
@@ -48,6 +48,10 @@
 #HINT 2: You'll probably want to use a dictionary, with station IDs as
 #the keys and 
 
+important_details_filename = "marta_sample.txt"
+stations_filename = "stations.txt"
+
+
 def packingStationNamesIntoDict(filename):
     stationNamesFile = open(filename, "r")
     fileContents = stationNamesFile.readlines()
@@ -66,8 +70,6 @@ def packingStationNamesIntoDict(filename):
         
     return dict_of_stations
 
-print(packingStationNamesIntoDict("stations.txt"))
-print()
 
 def six_important_details_in_one_dict(filename):
     marta_file = open(filename, 'r')
@@ -100,8 +102,8 @@ def six_important_details_in_one_dict(filename):
             "serialNumber":serialNumberList
             }
     return dict_of_details
-#print(six_important_details_in_one_dict("marta_sample.txt"))
-print("We're now currently working on avg_breezeCardTaps")
+
+
 def avg_breezeCardTaps(aDict): 
     cardTapsDictionary = {}
     
@@ -112,23 +114,67 @@ def avg_breezeCardTaps(aDict):
     for stationID in list_of_stationIDs: 
         number_of_taps_per_station = list_of_stationIDs.count(stationID)
         cardTapsDictionary[stationID] = number_of_taps_per_station
-    print("Our currentTapsDictionary currently looks like:", cardTapsDictionary)
 
     number_of_stations = len(cardTapsDictionary)
-    print("There are", number_of_stations, "stations.")
-        
     total_number_of_taps = 0
     for (stationID, number_of_taps) in cardTapsDictionary.items():
         total_number_of_taps += number_of_taps
-    print("There have been", total_number_of_taps, "overall!")
-
+        
     average_taps_per_station = total_number_of_taps / number_of_stations
+    average_taps_per_station = round(average_taps_per_station,2)
 
-    return average_taps_per_station
-    
+    return (average_taps_per_station, cardTapsDictionary)    
+
+
+def stationID_with_traffic_closest_to_average(important_details_local):
+    print("Now to find the station with the traffic closest to the average:")
+    if "stationID" in important_details_local:
+        list_of_stationIDs = important_details_local["stationID"]
+
+    overall_average = avg_breezeCardTaps(important_details_local)[0]
+    cardTapsDictionary = avg_breezeCardTaps(important_details_local)[1]
+    print("Our Overall average:", overall_average)
+    print("My cardtapsDictionary:", cardTapsDictionary)
     
 
-print(avg_breezeCardTaps(six_important_details_in_one_dict("marta_sample.txt")))
+    #Now I need to find the station with the taps closest to the average. 
+    #Firstly to find the differene between each number and our overage average:
+    my_dict_of_differences = {}
+    #Storing these differences in a dictionary
+    for (station, cardtaps) in cardTapsDictionary.items(): 
+        difference = overall_average-cardTapsDictionary[station]
+        difference = round(difference, 2)
+        difference = "difference: " + str(difference) 
+        my_dict_of_differences[station] = [cardtaps, difference]
+    
+    #For now I will use this dictionary to manually find the station with the total taps
+    #closest to our average. 
+    return my_dict_of_differences
+
+
+def find_station_with_least_traffic(closest_average, station_dict):
+
+    Updated_station_dict = {}
+    
+    for station_number, card_taps_tuple in closest_average.items():
+        Updated_station_dict[station_dict[station_number]] = card_taps_tuple
+    
+    return Updated_station_dict
+
+
+print(six_important_details_in_one_dict("marta_sample.txt"))
+
+# station_dict = packingStationNamesIntoDict("stations.txt")
+# important_details_dict = six_important_details_in_one_dict(important_details_filename)
+
+
+# average_card_taps_dict = avg_breezeCardTaps(important_details_dict)
+# closest_average_dict = stationID_with_traffic_closest_to_average(important_details_dict)
+
+# print(find_station_with_least_traffic(closest_average_dict, station_dict))
+
+
+
 
 
 
---
+++ marta.py
@@ -0,0 +1,240 @@
+import unittest
+
+#MARTA Breeze Card Taps
+#In this problem, we're giving you a file containing some real data from
+#the Marta (Atlanta's subway system) database. Each line of the file is
+#a record of a single ride at a specific Marta station. Riders enter and
+#exit the subway system by tapping a Breeze Card against a gate at a
+#specific station.
+#
+#You can see a preview of what the file will look like in
+#marta_sample.csv in the dropdown in the top left. Note, however, that
+#the real dataset is massive: over 200,000 individual rides are recorded.
+#So, you're going to be dealing with some pretty big data!
+
+#Note: NB: expect 200'000 lines of code (real file)
+#
+#Each line of the file contains six items, separated by commas:
+#
+# - the transit day, in MM/DD/YYYY format.
+# - the transit time, in HH:MM:SS format.
+# - the device ID, an identifer of the gate at which the rider entered.
+# - the station ID, a numeric identifier the station.
+# - the use type, whether the rider was entering, exiting, etc.
+# - a serial number, the unique identifier of the rider's Breeze Card.
+#
+#Your goal is to use this file to answer three questions:
+#
+# - What is the average number of Breeze Card taps per station?
+# - What is the station ID of the station whose traffic is the closest
+#   to that average?
+# - What station has the least overall amount of traffic?
+#
+#Note that you will answer these questions in the fill-in-the-blank
+#problems below, _not_ in this coding exercise. So, you don't have to
+#programmatically find the station ID closest to the average: you could
+#just print all the stations and their averages, then visually check
+#which is closest to the average.
+#
+#To get you started, we've gone ahead and opened the file:
+
+
+
+#You may add whatever code you want from here on to answer those three
+#questions.
+#
+#HINT: although there are six items on each line of the file, you only
+#need one of them: station ID. If you use split(",") to split up each
+#line, station ID will be at index 3 on the list.
+#
+#HINT 2: You'll probably want to use a dictionary, with station IDs as
+#the keys and 
+
+def load_stations(filename):
+    station_file = open(filename, "r")
+    file_contents = station_file.readlines()
+    station_file.close()
+
+    stations_dictionary = {} 
+
+    for line in file_contents:
+        split_line = line.split("\t")
+        station_number = split_line[0]
+        station_name = split_line[1].strip()
+        stations_dictionary[station_number] = station_name
+
+    return stations_dictionary
+
+
+def load_marta_file(filename):
+    marta_file = open(filename, "r")
+    file_contents = marta_file.readlines()
+    marta_file.close()
+
+    rides_per_station = {}
+    for line in file_contents:
+        split_line = line.split(",")
+        station_ID = split_line[3]
+        individual_ride = {
+            "day": split_line[0],
+            "time": split_line[1],
+            "device_id": split_line[2],
+            "station_id": station_ID,
+            "use_type": split_line[4],
+            "serial_number": split_line[5].strip()
+        }
+        if not station_ID in rides_per_station:
+            rides_per_station[station_ID] = []
+        rides_per_station[station_ID].append(individual_ride)
+
+    return rides_per_station
+
+
+def average_taps_per_stations(rides_per_station):
+    total_taps = 0.0
+    number_of_stations = len(rides_per_station)
+    
+    for rides in rides_per_station.values():
+        total_taps += len(rides)
+
+    return total_taps / number_of_stations
+
+
+def station_with_least_traffic(rides_per_station):
+    station_with_least_traffic = ""
+    minimal_number_of_trips = None
+
+    for station, rides in rides_per_station.items():
+        number_of_trips = len(rides)
+
+        if minimal_number_of_trips == None: 
+            minimal_number_of_trips = number_of_trips
+            station_with_least_traffic = station
+
+        if number_of_trips < minimal_number_of_trips: 
+            minimal_number_of_trips = number_of_trips
+            station_with_least_traffic = station
+
+    return station_with_least_traffic
+
+
+def get_total_trips_per_station(rides_per_station): 
+    trips_per_station = []
+    for station, rides in rides_per_station.items():
+        trips_per_station.append({
+            "station": station, 
+            "trips": len(rides)
+        })
+
+    return trips_per_station
+
+
+def find_closest_to_average(average_taps, rides_per_station):
+    closest_to_average = []
+    difference = None
+    for station, rides in rides_per_station.items():
+        current = abs(len(rides) - average_taps)
+        if difference == None: 
+            closest_to_average.append(station)
+            difference = current
+        elif difference == current: 
+            closest_to_average.append(station)
+        elif difference > current: 
+            closest_to_average = [station]
+            difference = current
+    
+    return closest_to_average
+
+
+def output_data():
+    filename = ''
+    rides_per_station = load_marta_file(filename)
+    average_taps = average_taps_per_stations(rides_per_station)
+    print("Average taps:", average_taps)
+    print("Least Traffic:", station_with_least_traffic(rides_per_station))
+    print("Closest to Average:", find_closest_to_average(average_taps, rides_per_station))
+
+
+# Code written by Andi, Tests written by Peter
+class TestMarta(unittest.TestCase):
+    def test_should_return_closest_to_average(self):
+        rides_per_station = load_marta_file('marta_sample.txt')
+        average_taps = average_taps_per_stations(rides_per_station)
+
+        actual = find_closest_to_average(average_taps, rides_per_station)
+
+        self.assertEqual(["30", "31"], actual)
+
+    # - What is the station ID of the station whose traffic is the closest
+    #   to that average?
+    def test_should_return_all_stations_and_total_trips(self):
+        rides_per_station = load_marta_file('marta_sample.txt')
+        output = [
+            { "station": "30", "trips": 6 },
+            { "station": "31", "trips": 4 }
+        ]
+
+        actual = get_total_trips_per_station(rides_per_station)
+
+        self.assertEqual(output, actual)
+
+    # - What station has the least overall amount of traffic?
+    def test_should_return_station_with_least_traffic(self):
+        rides_per_station = load_marta_file('marta_sample.txt')
+
+        actual = station_with_least_traffic(rides_per_station)
+
+        self.assertEqual("31", actual)
+
+    def test_should_return_ave_taps_of_5_for_file(self):
+        rides_per_station = load_marta_file('marta_sample.txt')
+
+        actual = average_taps_per_stations(rides_per_station)
+
+        self.assertEqual(5.0, actual)
+
+    def test_should_load_stations(self):
+        expected_stations = {
+            '00': 'MARTA Training',
+            '01': 'CTS Test Lab 1',
+            '02': 'Test Bus Facility',
+            '03': 'HPEM Lab'
+        }
+
+        result = load_stations('station_small.txt')
+
+        self.assertEqual(expected_stations, result)
+
+    def test_should_load_marta_file_correctly(self):
+        # - the transit day, in MM/DD/YYYY format.
+        # - the transit time, in HH:MM:SS format.
+        # - the device ID, an identifer of the gate at which the rider entered.
+        # - the station ID, a numeric identifier the station.
+        # - the use type, whether the rider was entering, exiting, etc.
+        # - a serial number, the unique identifier of the rider's Breeze Card.
+
+        def create_entry(day, time, device_id, station_id, use_type, serial_number):
+            return {
+                "day": day,
+                "time": time,
+                "device_id": device_id,
+                "station_id": station_id,
+                "use_type": use_type,
+                "serial_number": serial_number
+            }
+        
+        expected_marta = {
+            '30': [
+                create_entry('01/18/2016', '12:01:08', 'RVG10405', '30', '9', '3E8CADDB574A4E4A'),
+                create_entry('01/18/2016', '08:01:06', 'RVG10402', '30', '9', '3E8CADDF4A674E4A'),
+                create_entry('01/18/2016', '16:01:03', 'RVG10402', '30', '9', '3E8CADF935024E4A')
+            ]
+        }
+
+        actual = load_marta_file('small_marta.csv')
+
+        self.assertEqual(expected_marta, actual)
+        
+
+if __name__ == '__main__':
+    unittest.main()
