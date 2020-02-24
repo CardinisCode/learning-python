308ea5a9e50fa380822f6adb1acd685d0d1224ec
Andi Folgado <ac.folgado@gmail.com>
2020-02-24 12:32:05 +0000

    Updated & Solved Code Test Problem 5

--- codetestproblem5.py
+++ codetestproblem5.py
@@ -28,31 +28,36 @@
 
 #Add your code here!
 def next_fib(fibonacci_list, n):
+    pseudo_fibonacci = []
     fibonacci_length = len(fibonacci_list)
-    while fibonacci_length <= (fibonacci_length + 3): 
-        last_number = fibonacci_list[fibonacci_length -1]
-        print(last_number)
-        next_fibonacci = last_number + fibonacci_list[fibonacci_length -2]
-        fibonacci_list.append(next_fibonacci)
-        fibonacci_length += 1
+    print("The current list length has", fibonacci_length, "numbers")
+
+    for i in fibonacci_list:
+        pseudo_fibonacci.append(i)
+
+    first_number = fibonacci_list[fibonacci_length -2]
+    second_number =fibonacci_list[fibonacci_length -1] 
+    print("My first number:", first_number, "and my second number:", second_number)
     
-    #pseudo_fibonacci = []
-    #for i in fibonacci_list:
-        #pseudo_fibonacci.append(i)
+    for i in range(0, n):
+        next_number = first_number + second_number
+        pseudo_fibonacci.append(next_number)
+        first_number = second_number
+        second_number = next_number
+    
+    p_fibonacci_length = len(pseudo_fibonacci)
+    print("There are", p_fibonacci_length, "numbers in this list")
 
-    #p_fibonacci_length = len(pseudo_fibonacci)
-    #print("There are", p_fibonacci_length, "numbers in this list")
+    return pseudo_fibonacci
 
-
-    #return pseudo_fibonacci
 #Below are some lines of code that will test your function.
 #You can change the value of the variable(s) to test your
 #function with different inputs.
 #If your function works correctly, this will originally
 #print:
-#[5, 5, 10, 15, 25, 40, 65, 105, 170, 275] 
-a_list = [5, 5, 10, 15, 25, 40, 65]
-print(next_fib(a_list, 3))
+a_list = [8, 10]
+print(next_fib(a_list, 4))
+
 
 
 
