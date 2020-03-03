2eda00c73fcaa98ce60f52c122009e9e792de70d
Andi Folgado <ac.folgado@gmail.com>
2020-03-03 19:17:47 +0000

    General testing and playing around with data to solve a problem

---
+++ generaltesting.py
@@ -0,0 +1,9 @@
+gradebook = []
+newStudent = {"name": "Samantha", "homework": 95, "test": 92, "exam": 100}
+gradebook.append(newStudent)
+
+newStudent = {"name": "David", "homework": 90, "test":90, "exam":98}
+gradebook.append(newStudent)
+print(gradebook[0]["name"])
+
+#"name": "David", "homework": 90, "test":90, "exam":98
