b9b9629e2b57b63a97c101595007ac68f32b18e3
Andi Folgado <ac.folgado@gmail.com>
2020-03-13 10:49:43 +0000

    General Testing
    Where I play with general code to test out theories. Will keep changing.

--- generaltesting.py
+++ generaltesting.py
@@ -1,9 +1,30 @@
-gradebook = []
-newStudent = {"name": "Samantha", "homework": 95, "test": 92, "exam": 100}
-gradebook.append(newStudent)
+class Artist:
+    def __init__(self, name, label):
+        self.name = name
+        self.label = label
 
-newStudent = {"name": "David", "homework": 90, "test":90, "exam":98}
-gradebook.append(newStudent)
-print(gradebook[0]["name"])
+class Song:
+    def __init__(self, name, album, year, artist):
+        self.name = name
+        self.album = album
+        self.year = year
+        self.artist = artist 
 
-#"name": "David", "homework": 90, "test":90, "exam":98
+taylor_swift = Artist("Taylor Sugar", "Big Machine Records, LLC")
+first_song = Song("You Belong With Me", "Fearless", 2008, taylor_swift)
+second_song = Song("All Too Well", "Red", 2012, taylor_swift)
+
+print()
+print("Artist #1")
+print("Song name:", first_song.name)
+print("Album name:", first_song.album)
+print("Year:", first_song.year)
+print("Artist name:", first_song.artist.name)
+print("Artist' label:", first_song.artist.label)
+print()
+print("Artist #2")
+print("Song name:", second_song.name)
+print("Album name:", second_song.album)
+print("Year:", second_song.year)
+print("Artist name:", second_song.artist.name)
+print("Artist' label:", second_song.artist.label)
