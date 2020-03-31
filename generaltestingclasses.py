a79b53b6b679370712ecfbde3a170a4f54071c62
Andi Folgado <ac.folgado@gmail.com>
2020-03-31 17:16:02 +0100

    General code exercises

--- generaltestingclasses.py
+++ generaltestingclasses.py
@@ -1,36 +1,57 @@
-class Counter:
+class Furniture:
+    def __init__(self, mat):
+        self.actual_material = mat
+
+    def print_material(self):
+        print("I am made of", self.actual_material)
+
+class Chair(Furniture):
+    def __init__(self, mat="wood"):
+        super().__init__(mat)
+
+    def isSitable(self):
+        return False
+
+class SteelChair(Chair):
     def __init__(self):
-        self.count = 0
+        super().__init__("steel")
 
-def incrementor(c, num):
-    c.count = c.count + 1
-    print("c.count:", c.count)
-    num += 1
+    def isSitable(self):
+        return True
 
-def main():
-    counter = Counter()
-    num = 0
-    for x in range(0, 100):
-        incrementor(counter, num)
-        
-    return (counter.count, num)
+class WoodChair(Chair):
+    def __init__(self):
+        super().__init__("oak wood")
+    
+    def isSitable(self):
+        return True
 
-aTuple = main()
+class Sofa(Furniture):
+    def __init__(self):
+        super().__init__("cloth")
 
+"""
+        F
+      /    \
+    C       S
+   /  \ 
+SC      WC
+"""
 
-class Name: 
-    def __init__(self, firstname, lastname):
-        self.firstname = firstname
-        self.lastname = lastname
+furns = [
+    #Furniture("no idea"), 
+    Furniture(),
+    Chair(), 
+    SteelChair(), 
+    WoodChair(),
+    Sofa()
+]
 
-class Person:
-    def __init__(self, name, eyecolour, age):
-        self.name = name
-        self.eyecolour = eyecolour
-        self.age = age
+for f in furns:
+    f.print_material()
 
-
-myPerson = Person(Name("John", "Smith"), "brown", 32)
-print(myPerson.name.firstname)
-print(myPerson.age)
+    if isinstance(f, Chair):
+        print("Chair is sitable:", f.isSitable())
+    else:
+        print("not a chair")
     
