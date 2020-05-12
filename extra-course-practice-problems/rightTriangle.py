15458be66337718a4e36bdb123a3a8bc9141a881
Andi Folgado <ac.folgado@gmail.com>
2020-05-12 09:58:47 +0100

    Extra  course practice problem #7: Done

--- extra-course-practice-problems/rightTriangle.py
+++ extra-course-practice-problems/rightTriangle.py
@@ -34,7 +34,13 @@
 
 
 #Write your class here!
+class RightTriangle:
+    def __init__(self, opposite, adjacent):
+        self.opposite = opposite
+        self.adjacent = adjacent
 
+    def find_hypotenuse(self):
+        return (self.opposite **2 + self.adjacent**2) ** 0.5
 
 
 #The code below will test your function. If it works, it
