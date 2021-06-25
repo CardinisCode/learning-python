c230a30a74cdf34e3de31937da16f950b76c5192
Andi Folgado <ac.folgado@gmail.com>
2021-06-25 16:33:30 +0100

    Minor update

--- Python Code Challenges/barracks/main.py
+++ Python Code Challenges/barracks/main.py
@@ -4,15 +4,14 @@ import math
 class Solution:
 
     def run(self, n, k):
-        # nr_ways = 0
-
+        # Lets first check for the edge cases:        
         if n == 0 or k == 0:
             return 0
 
         if k == 1:
             return k
 
-        # 1 -> n
+        # Now we can just apply the math:
         nr_ways = 0
         power = math.ceil(n/k)
         nr_ways = k ** power
